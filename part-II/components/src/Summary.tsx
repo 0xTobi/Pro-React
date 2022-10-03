0c47d0a81410a4afd831967025618a3e6a409ce2
Tobi Aderibigbe <0xdegn@gmail.com>
2022-10-03 23:32:30 +0100

    fixed Summary.tsx

--- part-II/components/src/Summary.tsx
+++ part-II/components/src/Summary.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function createInnerElements(names: any) {
   let arrayElements = [];
   for (let name of names) {
-    arrayElements.push(<p key = {Math.random()}>{`${name} contains ${name.length} letters`}</p>);
+    arrayElements.push(<p key = {Math.floor(Math.random())}>{`${name} contains ${name.length} letters`}</p>);
   }
   console.log(arrayElements);
   return arrayElements;
