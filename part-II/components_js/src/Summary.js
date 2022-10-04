068874dc701fc6c09d35ae30bcc416b0f932d944
Tobi Aderibigbe <0xdegn@gmail.com>
2022-10-04 17:21:03 +0100

    modified Summary.js

--- part-II/components_js/src/Summary.js
+++ part-II/components_js/src/Summary.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-
 export default function Summary(props) {
-  return (
-    <>
-      <td>{props.index + 1}</td>
-      <td>{props.name}</td>
-      <td>{props.name.length}</td>
-    </>
-  )
+  let result =
+    props.name.length >= 4 ? (
+      <React.Fragment>
+        <td>{props.index + 1}</td>
+        <td>{props.name}</td>
+        <td>{props.name.length}</td>
+      </React.Fragment>
+    ) : null;
+
+  return result;
 }
+
+// A component must always retun a value, if there's nothing to return, simply return null.
