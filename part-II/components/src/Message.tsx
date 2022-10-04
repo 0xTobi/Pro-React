1edb4c7257a43cb362d3401b8226f7133fa75354
Tobi Aderibigbe <0xdegn@gmail.com>
2022-10-04 16:08:47 +0100

    Restructure

--- part-II/components/src/Message.tsx
+++ part-II/components/src/Message.tsx
@@ -1,23 +1,43 @@
 import React from "react";
 
 export const Message = (props: any) => {
-  if (props.name) {
-    return (
-      <h4
-        className={
-          props.name === "bob"
-            ? "p-0.5 bg-red-500/100 text-center text-white mb-1.5"
-            : "p-0.5 bg-green-500/100 text-center text-white mb-1.5"
-        }
-      >
-        {props.name} says {props.greeting}
-      </h4>
-    );
-  } else {
-    return (
-      <h4 className="p-0.5 bg-blue-500/100 text-center text-white mb-1.5">
-        {props.greeting}
-      </h4>
-    );
-  }
+  // if (props.name) {
+  //   return (
+  //     <h4
+  //       className={
+  //         props.name === "bob"
+  //           ? "p-0.5 bg-red-500/100 text-center text-white mb-1.5"
+  //           : "p-0.5 bg-green-500/100 text-center text-white mb-1.5"
+  //       }
+  //     >
+  //       {props.name} says {props.greeting}
+  //     </h4>
+  //   );
+  // } else {
+  //   return (
+  //     <h4 className="p-0.5 bg-blue-500/100 text-center text-white mb-1.5">
+  //       {props.greeting}
+  //     </h4>
+  //   );
+  // }
+
+  // (REWRITTEN USING TERNARY OPERATOR)
+
+  const message = props.name ? (
+    <h4
+      className={
+        props.name === "bob"
+          ? "p-0.5 bg-red-500/100 text-center text-white mb-1.5"
+          : "p-0.5 bg-green-500/100 text-center text-white mb-1.5"
+      }
+    >
+      {props.name} says {props.greeting}
+    </h4>
+  ) : (
+    <h4 className="p-0.5 bg-blue-500/100 text-center text-white mb-1.5">
+      {props.summary}
+    </h4>
+  );
+
+  return message;
 };
--- part-II/components/src/Summary.tsx
+++ part-II/components/src/Summary.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 
 function createInnerElements(names: any) {
-  let arrayElements = [];
-  for (let name of names) {
-    arrayElements.push(<p key = {Math.floor(Math.random())}>{`${name} contains ${name.length} letters`}</p>);
-  }
-  console.log(arrayElements);
-  return arrayElements;
+  // let arrayElements = [];
+  // for (let name of names) {
+  //   arrayElements.push(
+  //     <div key={Math.random()}>{`${name} contains ${name.length} letters`}</div>
+  //   );
+  // }
+  // return arrayElements;
+
+  return names.map((name: any) => (
+    <div key={Math.random()}>{`${name} contains ${name.length} letters`}</div>
+  ));
 }
 
 export default function Summary(props: any) {
+  // console.log(props.names);
+  // console.log(createInnerElements(props.names));
   return (
-    <h4 className="bg-info text-white text-center p-2">
-      {createInnerElements(props.names)[0]}
-    </h4>
+    <>
+      <h4 className="bg-info text-white text-center p-2">
+        {createInnerElements(props.names)}
+      </h4>
+      <p>name is tobi</p>
+    </>
   );
 }
