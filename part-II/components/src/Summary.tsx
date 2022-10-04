35aa6651db5740ac68768a373e1a02c49d9c4fc0
Tobi Aderibigbe <0xdegn@gmail.com>
2022-10-04 16:23:32 +0100

    _

--- part-II/components/src/Summary.tsx
+++ part-II/components/src/Summary.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 function createInnerElements(names: any) {
+  // USING THE FOR OF LOOP
   // let arrayElements = [];
   // for (let name of names) {
   //   arrayElements.push(
@@ -8,18 +9,23 @@ function createInnerElements(names: any) {
   //   );
   // }
   // return arrayElements;
-
-  return names.map((name: any) => (
-    <div key={Math.random()}>{`${name} contains ${name.length} letters`}</div>
-  ));
+  // USING THE MAP HGHER ORDER METHOD
+  // return names.map((name: any) => (
+  //   <div key={Math.random()}>{`${name} contains ${name.length} letters`}</div>
+  // ));
 }
 
 export default function Summary(props: any) {
   return (
-    <>
-      <h4 className="bg-info text-white text-center p-2">
-        {createInnerElements(props.names)}
-      </h4>
-    </>
+    <div>
+      
+      {/* <h4>{createInnerElements(props.names)}</h4> */}
+
+      {props.names.map((name: any) => {
+        <div
+          key={Math.random()}
+        >{`${name} contains ${name.length} letters`}</div>;
+      })}
+    </div>
   );
 }
