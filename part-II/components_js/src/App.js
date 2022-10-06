0b0aae0bee22c9b707245f14ad52093839355c8b
Tobi Aderibigbe <0xdegn@gmail.com>
2022-10-06 12:10:13 +0100

    Added promoteName function

--- part-II/components_js/src/App.js
+++ part-II/components_js/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import Summary from "./Summary";
-import TableAction from "./TableAction";
 import ReactDOM from "react-dom";
 
 let names = ["Bob", "Alice", "Dora"];
@@ -11,17 +10,14 @@ function reverseNames() {
   ReactDOM.render(<App />, document.getElementById("root"));
 }
 
+function promoteName(name) {
+  names = [name, ...names.filter((val) => val !== name)];
+  ReactDOM.render(<App />, document.getElementById("root"));
+}
+
 function App() {
   return (
-    // <div className="App">
-    //   <h1>Hello Adam</h1>
-    //   <Message greeting="Hello there" name="bob" />
-    //   <Message greeting="Hola" name={"Alice and Smith"} />
-    //   <Message summary="This is a Summary I made" />
-    //   <Summary names={["Bob", "Alice", "Dora"]} />
-    // </div>
-
-    <React.Fragment>
+    <table>
       <thead>
         <tr>
           <th>#</th>
@@ -33,13 +29,17 @@ function App() {
         {names.map((name, index) => {
           return (
             <tr key={name}>
-              <Summary index={index} name={name} />
+              <Summary
+                index={index}
+                name={name}
+                reverseCallback={reverseNames}
+                promoteCallback={promoteName}
+              />
             </tr>
           );
         })}
       </tbody>
-      <TableAction reverseCallback={reverseNames} />
-    </React.Fragment>
+    </table>
   );
 }
 
--- part-II/components_js/src/Summary.js
+++ part-II/components_js/src/Summary.js
@@ -3,16 +3,27 @@ import React from "react";
 export default function Summary(props) {
   // React props are read only, and cannot be altered from the child component.
   // props.name = `Name: ${props.name}`;
-  let result =
-    props.name.length >= 4 ? (
-      <React.Fragment>
-        <td>{props.index + 1}</td>
-        <td>{props.name}</td>
-        <td>{props.name.length}</td>
-      </React.Fragment>
-    ) : null;
-
-  return result;
+  return (
+    <React.Fragment>
+      <td>{props.index + 1}</td>
+      <td>{props.name}</td>
+      <td>{props.name.length}</td>
+      <td>
+        <button
+          className="btn btn-primary btn-sm"
+          onClick={props.reverseCallback}
+        >
+          Change
+        </button>
+        <button
+          className="btn btn-primary btn-sm"
+          onClick={() => props.promoteCallback(props.name)}
+        >
+          Promote
+        </button>
+      </td>
+    </React.Fragment>
+  );
 }
 
 // A component must always retun a value, if there's nothing to return, simply return null.
